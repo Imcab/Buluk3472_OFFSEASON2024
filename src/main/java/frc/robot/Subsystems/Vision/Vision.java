fdb9ca29d7dea027eb0f52cf77df0fa0afd54ff8
Imcab <142848134+Imcab@users.noreply.github.com>
2024-09-18 23:28:06 -0600

    visionIOtest1

--- src/main/java/frc/robot/Subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/Subsystems/Vision/Vision.java
@@ -2,21 +2,28 @@ package frc.robot.Subsystems.Vision;
 
 import org.littletonrobotics.junction.Logger;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase{
     private final VisionIO io;
     private final VisionIOInputsAutoLogged inputs = new VisionIOInputsAutoLogged();
 
+    AprilTagFieldLayout aprilTagFieldLayout; 
+
    public Vision(VisionIO io){
         this.io = io;
+        aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
    }
+
    public void periodic(){
-    
+
         io.updateInputs(inputs);
         Logger.processInputs("Vision/VisionInputs", inputs);
-        SmartDashboard.putNumber("NUM", 0.5);
+   }
+   public AprilTagFieldLayout gFieldLayout(){
+      return aprilTagFieldLayout;
    }
    public double getLimelightY(){
      return inputs.getLimelightY;
@@ -27,4 +34,5 @@ public class Vision extends SubsystemBase{
    public boolean LimelighttargetFound(){
      return inputs.LimetargetFound;
    }
+   
 } 
--- src/main/java/frc/robot/Subsystems/Vision/VisionIO.java
+++ src/main/java/frc/robot/Subsystems/Vision/VisionIO.java
@@ -3,16 +3,47 @@ package frc.robot.Subsystems.Vision;
 import org.littletonrobotics.junction.AutoLog;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
 
 public interface VisionIO {
     @AutoLog
     public static class VisionIOInputs{
+        
         public double getLimelightY = 0.0;
         public double getLimelightX = 0.0;
         public boolean LimetargetFound = false;
         public Pose2d LimePose2d = new Pose2d();
+
+        //CAMERA 1
         public boolean cam1targets = false;
         public int targetIDcam1 = 0;
+        public Transform2d cam1pose = new Transform2d();
+        public double cam1Latency = 0.0;
+        public double getCam1yaw = 0.0;
+        public double getCam1pitch = 0.0;
+        public double Cam1PoseAmbiguity = 0.0;
+        public Transform3d robot_toCam1 = new Transform3d();
+
+        //CAMERA 2
+        public boolean cam2targets = false;
+        public int targetIDcam2 = 0;
+        public Transform2d cam2pose = new Transform2d();
+        public double cam2Latency = 0.0;
+        public double getCam2yaw = 0.0;
+        public double getCam2pitch = 0.0;
+        public double Cam2PoseAmbiguity = 0.0;
+        public Transform3d robot_toCam2 = new Transform3d();
+
+        //CAMERA 3
+        public boolean cam3targets = false;
+        public int targetIDcam3 = 0;
+        public Transform2d cam3pose = new Transform2d();
+        public double cam3Latency = 0.0;
+        public double getCam3yaw = 0.0;
+        public double getCam3pitch = 0.0;
+        public double Cam3PoseAmbiguity = 0.0;
+        public Transform3d robot_toCam3 = new Transform3d();
 
     }
 
